df9a3bb7171092103ac6b7bb6c7593416209da4a
alandreivisson <alandreivisson@gmail.com>
2023-10-03 21:54:46 -0300

    Inveni - Projeto Base

---
+++ Persistence/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inveni.Persistence
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() 
+        {
+        }
+        public Contexto(DbContextOptions<Contexto> contextOptions) : base(contextOptions)
+        {
+        }
+    }
+}
